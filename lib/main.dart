ca67bec546b4559812292090186f6bdd65a6cd6d
leonardo silva <silvaleonardo.ti@gmail.com>
2020-11-02 14:05:53 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:buscador_gif/UI/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'UI/home_page.dart';
+import 'UI/gif_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
